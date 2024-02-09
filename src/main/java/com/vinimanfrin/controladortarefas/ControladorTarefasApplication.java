cb82f0dffcb449a87b5989d4604ee03e0821b60b
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-02-09 18:34:24 -0300

    criação do projeto

---
+++ src/main/java/com/vinimanfrin/controladortarefas/ControladorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.vinimanfrin.controladortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControladorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControladorTarefasApplication.class, args);
+	}
+
+}
