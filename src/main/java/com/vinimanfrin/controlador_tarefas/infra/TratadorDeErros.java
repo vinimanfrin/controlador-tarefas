43cfd921bfc6fd6216aa5a5708173a8755f8d9f1
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-02-12 12:13:28 -0300

    tratando erro 404

--- src/main/java/com/vinimanfrin/controlador_tarefas/infra/TratadorDeErros.java
+++ src/main/java/com/vinimanfrin/controlador_tarefas/infra/TratadorDeErros.java
@@ -1,5 +1,6 @@
 package com.vinimanfrin.controlador_tarefas.infra;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,5 +13,10 @@ public class TratadorDeErros {
     public ResponseEntity tratarErro400(MethodArgumentNotValidException e){
         var erros = e.getFieldErrors();
         return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+        return ResponseEntity.notFound().build();
     }
 }
