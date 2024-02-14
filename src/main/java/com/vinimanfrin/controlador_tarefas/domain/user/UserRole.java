b947633b394362e655838bc6f1c468f51a145bbd
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-02-14 12:00:09 -0300

    criando a entidade que representa os usuários

---
+++ src/main/java/com/vinimanfrin/controlador_tarefas/domain/user/UserRole.java
@@ -0,0 +1,15 @@
+package com.vinimanfrin.controlador_tarefas.domain.user;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+    UserRole(String role){
+        this.role = role;
+    }
+
+    public String getRole(){
+        return role;
+    }
+}
