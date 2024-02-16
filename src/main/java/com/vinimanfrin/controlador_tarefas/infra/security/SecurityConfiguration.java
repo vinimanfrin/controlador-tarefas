c8ec4c8555813cb6dbee8e1d53b24ef542ec809f
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-02-16 19:33:53 -0300

    Alterando as configurações de segurança para que o filtro que criei seja chamado antes do filtro do spring security

--- src/main/java/com/vinimanfrin/controlador_tarefas/infra/security/SecurityConfiguration.java
+++ src/main/java/com/vinimanfrin/controlador_tarefas/infra/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.vinimanfrin.controlador_tarefas.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,10 +11,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration{
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -24,6 +29,7 @@ public class SecurityConfiguration{
                     req.requestMatchers("/autenticacao/login").permitAll();
                     req.anyRequest().authenticated();
                 })
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
